18ea70a5d239adb89ba2b8daf7e6caf7340c2455
spcruz5 <spcruz5@gmail.com>
2022-09-09 17:43:00 +0000

    Program that prints num and list of argu

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
-if __name__ == '__main__':
-    import sys
-
-    Number_of_arguments = len(sys.argv)
-    if Number_of_arguments <= 1:
-        print(f"0 arguments.")
+from sys import argv
+if __name__ == "__main__":
+    i = 1
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[i]))
     else:
-        if len(sys.argv) == 2:
-            print(f"{len(sys.argv) - 1} argument:\n{len(sys.argv) - 1}:\
- {sys.argv[1]}")
-        elif len(sys.argv) > 2:
-            print(f"{len(sys.argv) - 1} arguments:")
-            for i in range(1, len(sys.argv)):
-                print(f"{i}: {sys.argv[i]}")
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
