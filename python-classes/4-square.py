e9c8b142f3f8bc5a0194c6a5048e8f3324a9e716
spcruz5 <spcruz5@gmail.com>
2022-09-27 18:04:13 -0400

    Fix space in task 3

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-""" Define a square class"""
-
-
 class Square:
-    """Square class"""
+    __doc__ = "Square Validation"
+
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        return (self.__size ** 2)
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        self.__size = value
+        if type(self.__size) == int:
+            if self.__size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                return self.__size
+        else:
             raise TypeError("size must be an integer")
-        self.__size = 
