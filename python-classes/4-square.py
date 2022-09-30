5730b81d0cf775d313b565662dfa5f3f72389c8b
spcruz5 <spcruz5@gmail.com>
2022-09-30 14:09:20 -0400

    fix error in code task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
+""" This is class named Square that defines a square """
+
+
 class Square:
-    __doc__ = "Square Validation"
+        """ definition """
+        def __init__(self, size=0):
+                """ Definition of private instance attribute size
+                which is asigned with the double underscore before the name
+                Args:
+                    size (int): private instance attribute.
+                Raises:
+                    TypeError: if `size` is not an integer
+                    ValueError: if `size` is less than zero
+                """
+                if type(size) is not int:
+                        raise TypeError("size must be an integer")
+                elif size < 0:
+                        raise ValueError("size must be >=0")
+                else:
+                        self.__size = size
 
-    def __init__(self, size=0):
-        self.__size = size
-
-    def area(self):
-        return (self.__size * self.__size)
-
-    @property
-    def size(self):
-        return (self.__size)
-
-    @size.setter
-    def size(self, value):
-        self.__size = value
-        if type(self.__size) == int:
-            if self.__size < 0:
-                raise ValueError("size must be >= 0")
-            else:
+        @property
+        def size(self):
+                """ getter function"""
                 return self.__size
-        else:
-            raise TypeError("size must be an integer")
+
+        @size.setter
+        def size(self, value):
+                """ setter func """
+                if type(value) is not int:
+                        raise TypeError("size must be an integer")
+                elif value < 0:
+                        raise ValueError("size must be >=0")
+                else:
+                        self.__size = value
+
+        def area(self):
+                """ Definition of function area that calculate
+                the area of the square"""
+                return self.__size ** 2
