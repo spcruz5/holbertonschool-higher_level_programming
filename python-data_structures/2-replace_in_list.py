5195bb7b7c743d752ed8213021192661cdd3f862
spcruz5 <spcruz5@gmail.com>
2022-10-02 23:24:46 -0400

    fix error in task 2 eliminate value list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+
+    if idx >= len(my_list):
+        return (my_list)
+
+    my_list[idx] = element
+    return(my_list)
+    
