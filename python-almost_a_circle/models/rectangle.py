b1772414090ab4ac6680414afbc23ec6509f6dfb
spcruz5 <spcruz5@gmail.com>
2022-10-05 09:59:58 -0400

    Fix setter in task 3 rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,33 +28,49 @@ class Rectangle(Base):
                         private attribute height
                 """
                 return self.__height
-        
+                      
+        @property
+        def x(self):
+                """ getter method of x"""
+                return self.__x
+
+        @property
+        def y(self):
+                """ getter method of y"""
+                return self.__y
+
         @width.setter
         def width(self, value):
-                """ Function that set the value of width
-                Args:
-                        value (int): width
-                Raises:
-                        TypeError: if value is not int
-                        ValueError: if value is less than 0
-                """
+                """ setter method of width"""
                 if type(value) is not int:
                         raise TypeError("width must be an integer")
-                if value < 0:
-                        raise ValueError("width must be >= 0")
+                if value <= 0:
+                        raise ValueError("width must be > 0")
                 self.__width = value
 
         @height.setter
         def height(self, value):
-                """ Function that set the value of height
-                Args:
-                        value (int): height
-                Raises:
-                        TypeError: if value is not int
-                        ValueError: if value is less than 0
-                """
+                """ setter method of height"""
                 if type(value) is not int:
                         raise TypeError("height must be an integer")
-                if value < 0:
-                        raise ValueError("height must be >= 0")
+                if value <= 0:
+                        raise ValueError("height must be > 0")
                 self.__height = value
+
+        @x.setter
+        def x(self, value):
+                """ setter method of x"""
+                if type(value) is not int:
+                        raise TypeError("x must be an integer")
+                if value < 0:
+                        raise ValueError("x must be >= 0")
+                self.__x = value
+
+        @y.setter
+        def y(self, value):
+                """ setter method of y"""
+                if type(value) is not int:
+                        raise TypeError("y must be an integer")
+                if value < 0:
+                        raise ValueError("y must be >= 0")
+                self.__y = value
