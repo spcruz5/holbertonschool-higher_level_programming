a87c2f1b293f14b83791038d536049d698731574
spcruz5 <spcruz5@gmail.com>
2022-10-05 10:09:50 -0400

    Adding Area to rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
                 if value < 0:
                         raise ValueError("y must be >= 0")
                 self.__y = value
+ 
+        def area(self):
+                """ return the area of the rectangle"""
+                return self.__height * self.__width
