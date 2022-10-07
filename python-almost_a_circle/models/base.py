1e1f4b88bd37c846a008b6a919ef4b3179b0874f
spcruz5 <spcruz5@gmail.com>
2022-10-06 21:23:04 -0400

    Base adding class method save to and static json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Class base"""
+import json
+import csv
+import os.path
 
 
 class Base:
@@ -13,3 +16,27 @@ class Base:
                 else:
                         Base.__nb_objects += 1
                         self.id = self.__nb_objects
+
+        @staticmethod
+        def to_json_string(list_dictionaries):
+            """ List to JSON string """
+            if list_dictionaries is None or list_dictionaries == "[]":
+                return "[]"
+            return json.dumps(list_dictionaries)
+
+        @classmethod
+        def save_to_file(cls, list_objs):
+            """ Save object in a file """
+            filename = "{}.json".format(cls.__name__)
+            list_dic = []
+
+            if not list_objs:
+                pass
+            else:
+                for i in range(len(list_objs)):
+                    list_dic.append(list_objs[i].to_dictionary())
+
+            lists = cls.to_json_string(list_dic)
+
+            with open(filename, 'w') as f:
+                f.write(lists)
