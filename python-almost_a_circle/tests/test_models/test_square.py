6935026599fd5bae474d67d499d98e1b1d027d59
spcruz5 <spcruz5@gmail.com>
2022-10-06 21:04:23 -0400

    Test 1 for square

---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Module for test Square class """
+import unittest
+from unittest import TestCase
+from models.square import Square
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestSquareMethods(unittest.TestCase):
+    """ Suite to test Square class """
+
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
+
+    def test_new_square(self):
+        """ Test new square """
+        new = Square(3)
+        self.assertEqual(new.size, 3)
+        self.assertEqual(new.width, 3)
+        self.assertEqual(new.height, 3)
+        self.assertEqual(new.x, 0)
+        self.assertEqual(new.y, 0)
+        self.assertEqual(new.id, 1)
+
+    def test_new_square_2(self):
+        """ Test new square with all attrs """
+        new = Square(2, 5, 5, 4)
+        self.assertEqual(new.size, 2)
+        self.assertEqual(new.width, 2)
+        self.assertEqual(new.height, 2)
+        self.assertEqual(new.x, 5)
+        self.assertEqual(new.y, 5)
+        self.assertEqual(new.id, 4)
+
+    def test_new_squares(self):
+        """ Test new squares """
+        new = Square(1, 1)
+        new2 = Square(1, 1)
+        self.assertEqual(False, new is new2)
+        self.assertEqual(False, new.id == new2.id)
+
+    def test_is_Base_instance(self):
+        """ Test Square is a Base instance """
+        new = Square(1)
+        self.assertEqual(True, isinstance(new, Base))
+
+    def test_is_Rectangle_instance(self):
+        """ Test Square is a Rectangle instance """
+        new = Square(1)
+        self.assertEqual(True, isinstance(new, Rectangle))
