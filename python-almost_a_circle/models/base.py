24a5672a2a16846fd198fa546f8fb6ec67c8fa91
spcruz5 <spcruz5@gmail.com>
2022-10-07 13:47:01 -0400

    adding class create to base for rec and squ

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,3 +40,20 @@ class Base:
 
             with open(filename, 'w') as f:
                 f.write(lists)
+
+        @staticmethod
+        def from_json_string(json_string):
+            """ JSON string to dictionary """
+            if not json_string:
+                return []
+            return json.loads(json_string)
+
+        @classmethod
+        def create(cls, **dictionary):
+            """ Create an instance """
+            if cls.__name__ == "Rectangle":
+                new = cls(10, 10)
+            else:
+                new = cls(10)
+            new.update(**dictionary)
+            return new
